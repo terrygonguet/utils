f1fe4257a3ace21aaeb587a94a518f7c36195b96
terrygonguet <terry@gonguet.com>
2023-06-10 01:48:29 +0200

    test(main): :white_check_mark: Slightly prettier tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,15 +1,15 @@
-import { describe, expect } from "vitest";
+import { describe } from "vitest";
 import { clamp } from "./main.ts"
 
 describe.concurrent("clamp", it => {
-	it("Does nothing to values within range", () => {
+	it("Does nothing to values within range", ({ expect }) => {
 		expect(clamp(5, 0, 10)).to.equal(5)
 		expect(clamp(10, 10, 10)).to.equal(10)
 		expect(clamp(0, 0, 10)).to.equal(0)
 		expect(clamp(10, 0, 10)).to.equal(10)
 	})
 
-	it("Clamps values outside of range", () => {
+	it("Clamps values outside of range", ({ expect }) => {
 		expect(clamp(-5, 0, 10)).to.equal(0)
 		expect(clamp(15, 0, 10)).to.equal(10)
 		expect(clamp(-5, 10, 10)).to.equal(10)
