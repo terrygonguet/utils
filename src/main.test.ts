62cdc3e2fcc9a10a0a4b230d4f627ca35d314853
terrygonguet <terry@gonguet.com>
2023-06-10 01:12:21 +0200

    test(main): :white_check_mark: Add tests to `clamp`

---
+++ src/main.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect } from "vitest";
+import { clamp } from "./main.ts"
+
+describe.concurrent("clamp", it => {
+	it("Does nothing to values within range", () => {
+		expect(clamp(5, 0, 10)).to.equal(5)
+		expect(clamp(10, 10, 10)).to.equal(10)
+		expect(clamp(0, 0, 10)).to.equal(0)
+		expect(clamp(10, 0, 10)).to.equal(10)
+	})
+
+	it("Clamps values outside of range", () => {
+		expect(clamp(-5, 0, 10)).to.equal(0)
+		expect(clamp(15, 0, 10)).to.equal(10)
+		expect(clamp(-5, 10, 10)).to.equal(10)
+		expect(clamp(15, 10, 10)).to.equal(10)
+	})
+})
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+/**
+ * Behaviour is undefined when max < min
+ */
 export function clamp(value: number, min: number, max: number) {
 	return Math.min(Math.max(value, min), max)
 }
