649a0b69553b0a3d6fd15131cbe6cd3d0305336d
terrygonguet <terry@gonguet.com>
2023-06-10 16:20:22 +0200

    test: :white_check_mark: Update tests

--- src/functional/index.test.js
+++ src/functional/index.test.js
@@ -1,7 +1,7 @@
 import { describe } from "vitest"
 import { identity } from "./index.ts"
 
-describe("identity()", it => {
+describe.concurrent("identity()", it => {
 	it("does nothing", ({ expect }) => {
 		expect(identity(5)).to.equal(5)
 		expect(identity("test")).to.equal("test")
