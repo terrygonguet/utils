98f63454a0be5f296be319d10116c822f6221b2c
terrygonguet <terry@gonguet.com>
2023-06-11 01:14:35 +0200

    feat(functional): :sparkles: Add `constant()`
    + tests

--- src/functional/index.test.ts
+++ src/functional/index.test.ts
@@ -1,5 +1,5 @@
 import { describe } from "vitest"
-import { identity } from "./index.ts"
+import { constant, identity } from "./index.ts"
 
 describe.concurrent("identity()", it => {
 	it("does nothing", ({ expect }) => {
@@ -7,5 +7,13 @@ describe.concurrent("identity()", it => {
 		expect(identity("test")).to.equal("test")
 		const object = { prop: "value" }
 		expect(identity(object)).to.equal(object)
+	})
+})
+
+describe.concurrent("constant()", it => {
+	it("is constant", ({ expect }) => {
+		const five = constant(5)
+		expect(five()).to.equal(5)
+		expect(five()).to.equal(5)
 	})
 })
--- src/functional/index.ts
+++ src/functional/index.ts
@@ -5,3 +5,7 @@ export { default as compose } from "just-compose"
 export function identity<T>(value: T) {
 	return value
 }
+
+export function constant<T>(value: T) {
+	return () => value
+}
