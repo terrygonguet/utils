1186a185a9a0f3144fd2246c8dd745b9f4bdd847
terrygonguet <terry@gonguet.com>
2023-06-10 16:44:43 +0200

    feat(main): :sparkles: Add safeParse function
    + tests

--- src/main.test.js
+++ src/main.test.js
@@ -1,18 +1,43 @@
 import { describe } from "vitest"
-import { clamp } from "./main.ts"
+import { clamp, safeParse } from "./main.ts"
 
-describe.concurrent("clamp", it => {
-	it("Does nothing to values within range", ({ expect }) => {
+describe.concurrent("clamp()", it => {
+	it("does nothing to values within range", ({ expect }) => {
 		expect(clamp(5, 0, 10)).to.equal(5)
 		expect(clamp(10, 10, 10)).to.equal(10)
 		expect(clamp(0, 0, 10)).to.equal(0)
 		expect(clamp(10, 0, 10)).to.equal(10)
 	})
 
-	it("Clamps values outside of range", ({ expect }) => {
+	it("clamps values outside of range", ({ expect }) => {
 		expect(clamp(-5, 0, 10)).to.equal(0)
 		expect(clamp(15, 0, 10)).to.equal(10)
 		expect(clamp(-5, 10, 10)).to.equal(10)
 		expect(clamp(15, 10, 10)).to.equal(10)
+	})
+})
+
+describe.concurrent("safeParse()", it => {
+	it("works the same as JSON.parse for valid objects", ({ expect }) => {
+		const json = `{"prop":"value"}`
+		expect(safeParse(json, {})).to.deep.equal(JSON.parse(json))
+	})
+
+	it("uses default value for malformed JSON", ({ expect }) => {
+		const json = `{"prop":}`
+		expect(safeParse(json, { prop: "default" })).to.deep.equal({ prop: "default" })
+	})
+
+	it("uses revivers", ({ expect }) => {
+		const json = `{"prop":"value","other":"ignored"}`
+		function reviver(key, value) {
+			if (key == "prop") return "replaced"
+			else return value
+		}
+		const defaultValue = { prop: "value", other: "ignored" }
+		expect(safeParse(json, defaultValue, reviver)).to.deep.equal({
+			prop: "replaced",
+			other: "ignored",
+		})
 	})
 })
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,15 @@
 export function clamp(value: number, min: number, max: number) {
 	return Math.min(Math.max(value, min), max)
 }
+
+export function safeParse<T>(
+	str: string,
+	defaultValue: T,
+	reviver?: (key: string, value: any) => any,
+) {
+	try {
+		return JSON.parse(str, reviver)
+	} catch (_) {
+		return defaultValue
+	}
+}
