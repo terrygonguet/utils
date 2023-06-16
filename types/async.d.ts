a2ef55afc3765af97ccc64bc6de2004d2aa5f399
terrygonguet <terry@gonguet.com>
2023-06-16 21:41:13 +0200

    chore(async): :label: Sync types

--- types/async.d.ts
+++ types/async.d.ts
@@ -1,1 +1,8 @@
 export declare function pause(ms: number): Promise<void>;
+interface RetryOptions {
+    count?: number;
+    delay?: number | ((retryCount: number, error: any) => number);
+}
+export declare function retry(options: RetryOptions): <T>(provider: () => Promise<T>) => Promise<T>;
+export declare function retry<T>(provider: () => Promise<T>, options?: RetryOptions): Promise<T>;
+export {};
