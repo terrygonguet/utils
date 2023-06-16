7b108571f0e14f03e22a04cb6b3a2edf33ef833e
terrygonguet <terry@gonguet.com>
2023-06-16 21:40:33 +0200

    feat(async): :sparkles: Add `retry()`
    + tests

--- src/async.test.ts
+++ src/async.test.ts
@@ -1,11 +1,83 @@
 import { describe } from "vitest"
-import { pause } from "./async.ts"
+import { pause, retry } from "./async.ts"
 
-describe("pause()", it => {
-	it("Pauses", async ({ expect }) => {
+describe.concurrent("pause()", it => {
+	it("pauses", async ({ expect }) => {
 		const start = performance.now()
 		await pause(25)
 		const delta = performance.now() - start
 		expect(delta).to.be.greaterThanOrEqual(25)
+	})
+})
+
+describe.concurrent("retry()", _ => {
+	function makeFailN(n: number) {
+		return () => (n-- == 0 ? Promise.resolve("success") : Promise.reject("reason"))
+	}
+
+	describe.concurrent("funtional style", it => {
+		it("retries until the promise fulfils", async ({ expect }) => {
+			const failThrice = makeFailN(3)
+			const retryForever = retry({})
+			const result = await retryForever(failThrice)
+			expect(result).to.equal("success")
+		})
+
+		it("rejects with latest error when over count", async ({ expect }) => {
+			const fail10 = makeFailN(10)
+			const retryThrice = retry({ count: 3 })
+			await expect(retryThrice(fail10)).rejects.toThrow("reason")
+		})
+
+		it("waits between attemps by a flat delay", async ({ expect }) => {
+			const fail3 = makeFailN(3)
+			const retryWithDelay = retry({ delay: 15 })
+			const start = performance.now()
+			const result = await retryWithDelay(fail3)
+			expect(result).to.equal("success")
+			const delta = performance.now() - start
+			expect(delta).to.be.greaterThanOrEqual(3 * 15)
+		})
+
+		it("waits between attempts by a variable delay", async ({ expect }) => {
+			const fail3 = makeFailN(3)
+			const retryWithDelay = retry({ delay: n => n * 10 })
+			const start = performance.now()
+			const result = await retryWithDelay(fail3)
+			expect(result).to.equal("success")
+			const delta = performance.now() - start
+			expect(delta).to.be.greaterThanOrEqual(10 + 20 + 30)
+		})
+	})
+
+	describe.concurrent("imperative style", it => {
+		it("retries until the promise fulfils", async ({ expect }) => {
+			const failThrice = makeFailN(3)
+			const result = await retry(failThrice)
+			expect(result).to.equal("success")
+		})
+
+		it("rejects with latest error when over count", async ({ expect }) => {
+			const fail10 = makeFailN(10)
+			await expect(retry(fail10, { count: 5 })).rejects.toThrow("reason")
+		})
+
+		it("waits between attemps by a flat delay", async ({ expect }) => {
+			const fail3 = makeFailN(3)
+			const start = performance.now()
+			const result = await retry(fail3, { delay: 15 })
+			expect(result).to.equal("success")
+			const delta = performance.now() - start
+			expect(delta).to.be.greaterThanOrEqual(3 * 15)
+		})
+
+		it("waits between attempts by a variable delay", async ({ expect }) => {
+			const fail3 = makeFailN(3)
+			const start = performance.now()
+			const result = await retry(fail3, { delay: n => n * 10 })
+			expect(result).to.equal("success")
+			const delta = performance.now() - start
+			expect(delta).to.be.greaterThanOrEqual(10 + 20 + 30)
+		})
 	})
 })
--- src/async.ts
+++ src/async.ts
@@ -1,3 +1,43 @@
 export function pause(ms: number): Promise<void> {
 	return new Promise(resolve => setTimeout(resolve, ms))
 }
+
+interface RetryOptions {
+	count?: number
+	delay?: number | ((retryCount: number, error: any) => number)
+}
+
+export function retry(options: RetryOptions): <T>(provider: () => Promise<T>) => Promise<T>
+export function retry<T>(provider: () => Promise<T>, options?: RetryOptions): Promise<T>
+export function retry<T>(
+	providerOrOptions: RetryOptions | (() => Promise<T>),
+	options?: RetryOptions,
+) {
+	if (typeof providerOrOptions == "function") return _retry(providerOrOptions, options)
+	else return (provider: () => Promise<T>) => _retry(provider, providerOrOptions)
+}
+
+async function _retry<T>(
+	provider: () => Promise<T>,
+	{ count = Infinity, delay }: RetryOptions = {},
+): Promise<T> {
+	let retryCount = 0
+	let lastError: any
+	do {
+		try {
+			return await provider()
+		} catch (error) {
+			lastError = error
+			retryCount++
+			switch (typeof delay) {
+				case "number":
+					await pause(delay)
+					break
+				case "function":
+					await pause(delay(retryCount, error))
+					break
+			}
+		}
+	} while (retryCount <= count)
+	throw lastError
+}
