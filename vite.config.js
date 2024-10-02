8f20aee053f9accbc451633b3a769faabd8268ec
Terry Gonguet <terry@gonguet.com>
2024-10-02 23:52:29 +0200

    build: :wrench: Remove vite build config now that we use tsup

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { resolve } from "path"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-	build: {
-		lib: {
-			formats: ["es"],
-			entry: {
-				index: resolve(__dirname, "src/index.ts"),
-				async: resolve(__dirname, "src/async.ts"),
-				functional: resolve(__dirname, "src/functional/index.ts"),
-			},
-		},
-	},
 	test: {
 		coverage: {
 			reporter: ["lcov", "html"],
