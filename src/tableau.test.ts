374a7b061b667ff049ef9a658d17c5188159eb05
Terry Gonguet <terry@gonguet.com>
2024-10-03 15:04:24 +0200

    feat: :sparkles: Add 1 based tableau methods

--- src/tableau.test.ts
+++ src/tableau.test.ts
@@ -10,9 +10,113 @@ describe.concurrent("Tableau", it => {
 		expect(tab.i(3)).to.equal(3)
 	})
 
-	it("i() throws when accessing 0", ({ expect }) => {
+	it("throws when accessing 0", ({ expect }) => {
 		const tab = Tableau.of(1, 2, 3)
-		expect(() => tab.i(0)).to.toThrowError(TypeError)
+		expect(() => tab.i(0)).toThrowError(RangeError)
+		expect(() => tab.iindexOf(1, 0)).toThrowError(RangeError)
+		expect(() => tab.ilastIndexOf(1, 0)).toThrowError(RangeError)
+		expect(() => tab.isplice(0, 1)).toThrowError(RangeError)
+		expect(() => tab.iwith(0, 0)).toThrowError(RangeError)
+	})
+
+	it("ientries() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(Array.from(tab.ientries())).to.deep.equal([
+			[1, 1],
+			[2, 2],
+			[3, 3],
+		])
+	})
+
+	it("ievery() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.ievery((val, i) => val == i)).to.be.true
+	})
+
+	it("ifilter() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.ifilter((value, i) => value == i)).to.deep.equal([1, 2, 3])
+	})
+
+	it("ifind() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.ifind((_, i) => i == 3)).to.equal(3)
+		expect(tab.ifindLast((_, i) => i == 3)).to.equal(3)
+	})
+
+	it("ifindIndex() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.ifindIndex(value => value == 2)).to.equal(2)
+		expect(tab.ifindLastIndex(value => value == 2)).to.equal(2)
+	})
+
+	it("iflatMap() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.iflatMap((_, i) => Tableau.of(i + ""))).to.deep.equal([
+			"1",
+			"2",
+			"3",
+		])
+	})
+
+	it("iindexOf() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.iindexOf(2, 2)).to.equal(2)
+		expect(tab.iindexOf(2, 3)).to.be.undefined
+		expect(tab.ilastIndexOf(2, 2)).to.equal(2)
+		expect(tab.ilastIndexOf(2, 1)).to.be.undefined
+	})
+
+	it("iindexOf() retuns undefined when not found", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.iindexOf(5)).to.be.undefined
+		expect(tab.ilastIndexOf(5)).to.be.undefined
+	})
+
+	it("ikeys() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(Array.from(tab.ikeys())).to.deep.equal([1, 2, 3])
+	})
+
+	it("imap() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.imap((_, i) => i)).to.deep.equal([1, 2, 3])
+	})
+
+	it("ireduce() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.ireduce((sum, _, i) => sum + i, 0)).to.equal(6)
+		expect(tab.ireduceRight((sum, _, i) => sum + i, 0)).to.equal(6)
+	})
+
+	it("isome() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.isome((_, i) => i >= 3)).to.be.true
+	})
+
+	it("isort() does not mutate", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		const sorted = tab.isort((a, b) => b - a)
+		expect(tab).to.deep.equal([1, 2, 3])
+		expect(sorted).to.deep.equal([3, 2, 1])
+		expect(tab).to.not.equal(sorted)
+	})
+
+	it("isplice() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.isplice(2, 1, 2.5)).to.deep.equal([1, 2.5, 3])
+	})
+
+	it("isplice() does not mutate", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		const spliced = tab.isplice(2, 1, 2.5)
+		expect(tab).to.deep.equal([1, 2, 3])
+		expect(tab).to.not.equal(spliced)
+	})
+
+	it("iwith() is 1 based", ({ expect }) => {
+		const tab = Tableau.of(1, 2, 3)
+		expect(tab.iwith(2, 2.5)).to.deep.equal([1, 2.5, 3])
 	})
 
 	it("Tableau.from() constructs a new object", ({ expect }) => {
@@ -26,14 +130,5 @@ describe.concurrent("Tableau", it => {
 	it("Tableau.of() constructs a new object", ({ expect }) => {
 		const tab = Tableau.of(1, 2, 3)
 		expect(tab).to.deep.equal([1, 2, 3])
-	})
-
-	it("iterate() is 1 based", ({ expect }) => {
-		const tab = Tableau.of(1, 2, 3)
-		expect(Array.from(tab.iterate())).to.deep.equal([
-			[1, 1],
-			[2, 2],
-			[3, 3],
-		])
 	})
 })
--- src/tableau.ts
+++ src/tableau.ts
@@ -1,17 +1,199 @@
+type Predicate<T> = (value: T, index: number, tableau: Tableau<T>) => boolean
+type AssertPredicate<T, U extends T> = (
+	value: T,
+	index: number,
+	tableau: Tableau<T>,
+) => value is U
+type Reducer<T, U> = (
+	previousValue: U,
+	currentValue: T,
+	index: number,
+	tableau: Tableau<T>,
+) => U
+type Mapper<T, U> = (value: T, index: number, tableau: Tableau<T>) => U
+type Comparer<T> = (a: T, b: T) => number
+
+// Stolen from TS std lib
+type FlatTableau<Arr, Depth extends number> = {
+	done: Arr
+	recur: Arr extends Tableau<infer InnerArr>
+		? FlatTableau<
+				InnerArr,
+				[
+					-1,
+					0,
+					1,
+					2,
+					3,
+					4,
+					5,
+					6,
+					7,
+					8,
+					9,
+					10,
+					11,
+					12,
+					13,
+					14,
+					15,
+					16,
+					17,
+					18,
+					19,
+					20,
+				][Depth]
+		  >
+		: Arr
+}[Depth extends -1 ? "done" : "recur"]
+
 export class Tableau<T> extends Array<T> {
 	constructor(length?: number) {
 		length == undefined ? super() : super(length)
 	}
 
 	i(idx: number) {
-		if (idx == 0) throw new TypeError("Cannot access index 0")
+		if (idx == 0) throw new RangeError("Cannot access index 0 in a Tableau")
 		return this.at(idx - 1)
 	}
 
-	*iterate(): IterableIterator<[number, T]> {
+	*ientries(): IterableIterator<[number, T]> {
 		for (let i = 0; i < this.length; i++) {
 			yield [i + 1, this[i]]
 		}
+	}
+
+	ievery<U extends T>(
+		predicate: AssertPredicate<T, U>,
+		thisArg?: any,
+	): this is Tableau<U>
+	ievery(predicate: Predicate<T>, thisArg?: any): boolean
+	ievery(predicate: Predicate<T>, thisArg?: any) {
+		return this.every((value, i) => predicate(value, i + 1, this), thisArg)
+	}
+
+	ifilter<U extends T>(
+		predicate: AssertPredicate<T, U>,
+		thisArg?: any,
+	): Tableau<U>
+	ifilter(predicate: Predicate<T>, thisArg?: any): Tableau<T>
+	ifilter(predicate: Predicate<T>, thisArg?: any) {
+		return this.filter((value, i) => predicate(value, i + 1, this), thisArg)
+	}
+
+	ifind<U extends T>(
+		predicate: AssertPredicate<T, U>,
+		thisArg?: any,
+	): U | undefined
+	ifind(predicate: Predicate<T>, thisArg?: any): T | undefined
+	ifind(predicate: Predicate<T>, thisArg?: any) {
+		return this.find((value, i) => predicate(value, i + 1, this), thisArg)
+	}
+
+	ifindIndex(predicate: Predicate<T>, thisArg?: any): number | undefined {
+		const idx = this.findIndex(
+			(value, i) => predicate(value, i + 1, this),
+			thisArg,
+		)
+		return idx == -1 ? undefined : idx + 1
+	}
+
+	ifindLast<U extends T>(
+		predicate: AssertPredicate<T, U>,
+		thisArg?: any,
+	): U | undefined
+	ifindLast(predicate: Predicate<T>, thisArg?: any): T | undefined
+	ifindLast(predicate: Predicate<T>, thisArg?: any) {
+		return this.findLast(
+			(value, i) => predicate(value, i + 1, this),
+			thisArg,
+		)
+	}
+
+	ifindLastIndex(predicate: Predicate<T>, thisArg?: any): number | undefined {
+		const idx = this.findLastIndex(
+			(value, i) => predicate(value, i + 1, this),
+			thisArg,
+		)
+		return idx == -1 ? undefined : idx + 1
+	}
+
+	iflat<Depth extends number = 1>(depth?: Depth): FlatTableau<T, Depth> {
+		return this.flat(depth) as FlatTableau<T, Depth>
+	}
+
+	iflatMap<U>(
+		callback: Mapper<T, U | Tableau<U>>,
+		thisArg?: any,
+	): Tableau<U> {
+		return this.flatMap(
+			(value, i) => callback(value, i + 1, this),
+			thisArg,
+		) as Tableau<U>
+	}
+
+	iindexOf(searchElement: T, fromIndex = 1): number | undefined {
+		if (fromIndex == 0)
+			throw new RangeError("Cannot access index 0 in a Tableau")
+		const idx = this.indexOf(searchElement, fromIndex - 1)
+		return idx == -1 ? undefined : idx + 1
+	}
+
+	*ikeys(): IterableIterator<number> {
+		for (const key of this.keys()) {
+			yield key + 1
+		}
+	}
+
+	ilastIndexOf(
+		searchElement: T,
+		fromIndex = this.length,
+	): number | undefined {
+		if (fromIndex == 0)
+			throw new RangeError("Cannot access index 0 in a Tableau")
+		const idx = this.lastIndexOf(searchElement, fromIndex - 1)
+		return idx == -1 ? undefined : idx + 1
+	}
+
+	imap<U>(callbackFn: Mapper<T, U>, thisArg?: any): Tableau<U> {
+		return this.map(
+			(value, i) => callbackFn(value, i + 1, this),
+			thisArg,
+		) as Tableau<U>
+	}
+
+	ireduce<U>(callbackFn: Reducer<T, U>, initialValue: U): U {
+		return this.reduce(
+			(acc, cur, i) => callbackFn(acc, cur, i + 1, this),
+			initialValue,
+		)
+	}
+
+	ireduceRight<U>(callbackFn: Reducer<T, U>, initialValue: U): U {
+		return this.reduceRight(
+			(acc, cur, i) => callbackFn(acc, cur, i + 1, this),
+			initialValue,
+		)
+	}
+
+	isome(predicate: Predicate<T>, thisArg?: any): boolean {
+		return this.some((value, i) => predicate(value, i + 1, this), thisArg)
+	}
+
+	isort(compareFn: Comparer<T>): Tableau<T> {
+		return this.toSorted(compareFn) as Tableau<T>
+	}
+
+	isplice(start: number, deleteCount?: number, ...items: T[]): Tableau<T> {
+		if (start == 0)
+			throw new RangeError("Cannot access index 0 in a Tableau")
+		return this.toSpliced(start - 1, deleteCount!, ...items) as Tableau<T>
+	}
+
+	iwith(index: number, value: T): Tableau<T> {
+		if (index == 0)
+			throw new RangeError("Cannot access index 0 in a Tableau")
+		return this.with(index - 1, value) as Tableau<T>
 	}
 
 	static from<T>(source: Iterable<T> | ArrayLike<T>): Tableau<T> {
